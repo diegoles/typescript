e7a269aa6758b51149c0dd341f0082181477c27e
Edgar Diaz <ediaz@ec.krugercorp.com>
2019-12-26 12:00:43 -0500

    Interfaces, es un contrato que se debe cumplir, en este caso debe obligatoriamente tener la clase Coche los métodos de la Interface. Protected: se puede acceder desde la propia clase y desde sus hijas. Private: Solo puede acceder desde su propia clase.

--- coche.ts
+++ coche.ts
@@ -1,4 +1,9 @@
-class Coche {
+interface CocheBase {
+    getModelo(): string;
+    getVelocidad(): number;
+}
+
+class Coche implements CocheBase {
 
     public color: string;
     public modelo: string;
