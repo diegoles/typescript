0c4eae291af8cfbf4dd28e0c05bc995ab05c143c
Edgar Diaz <ediaz@ec.krugercorp.com>
2019-12-26 13:51:27 -0500

    Ejemplo práctico pequeño.

--- programa.ts
+++ programa.ts
@@ -42,3 +42,28 @@ editor.setVersion(8);
 editor.setTimeLine(4000);
 
 console.log(editor.getAllData());
+
+
+
+//Logica del formulario
+
+var programas = [];
+
+function guardar(){
+    var nombre = (<HTMLInputElement> document.getElementById("nombre")).value.toString();
+    var programa = new Programa();
+    programa.setNombre(nombre);
+    programa.setVersion(1);
+
+    programas.push(programa);
+
+    var list = "";
+    for(var i=0; i<programas.length; i++) {
+        list = list + "<li>" + programas[i].getNombre() + "</li>";
+    }
+
+    var listado = <HTMLElement> document.getElementById("listado");
+    listado.innerHTML = list;
+
+    (<HTMLInputElement> document.getElementById("nombre")).value = "";
+}
